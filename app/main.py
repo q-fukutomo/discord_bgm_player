9ebb28001f0e3f053716180b65f966f8519004d0
q-fukutomo <katatsuki52@gmail.com>
2021-02-12 21:24:08 +0900

    クォートの統一、変数名変更

--- app/main.py
+++ app/main.py
@@ -17,28 +17,28 @@ async def on_message(message):
     if message.author.bot:
         return
 
-    if message.content == "!join-bg":
+    if message.content == '!join-bg':
         if message.author.voice is None:
-            await message.channel.send(message.author.name + "は音声チャンネルに接続していません。")
+            await message.channel.send(message.author.name + 'は音声チャンネルに接続していません。')
             return
 
         await message.author.voice.channel.connect()
-        await message.channel.send("音声チャンネル`" +message.author.voice.channel.name+ "`に接続しました。")
+        await message.channel.send('音声チャンネル`' + message.author.voice.channel.name + '`に接続しました。')
         return
     
-    if message.content == "!quit-bg":
+    if message.content == '!quit-bg':
         if message.guild.voice_client is None:
-            await message.channel.send("音声チャンネルに接続していません。")
+            await message.channel.send('音声チャンネルに接続していません。')
             return
 
         channel = message.guild.voice_client.channel.name
         await message.guild.voice_client.disconnect()
-        await message.channel.send("音声チャンネル`" +channel+ "`から切断しました。")
+        await message.channel.send('音声チャンネル`' + channel + '`から切断しました。')
         return 
 
-    if re.match("!play-bg.*", message.content):
+    if re.match('!play-bg.*', message.content):
         if message.guild.voice_client is None:
-            await message.channel.send("音声チャンネルに接続していません。")
+            await message.channel.send('音声チャンネルに接続していません。')
             return
 
         result = re.match('!play-bg\s+(.+)', message.content)
@@ -52,30 +52,33 @@ async def on_message(message):
                 message.guild.voice_client.play( source )
                 return
 
-            await message.channel.send("音声ファイルが見つかりませんでした。")
+            await message.channel.send('音声ファイルが見つかりませんでした。')
             return
 
-        await message.channel.send("音声ファイルを指定してください。")
+        await message.channel.send('音声ファイルを指定してください。')
         return
 
-    if message.content == "!stop-bg":
+    if message.content == '!stop-bg':
         if message.guild.voice_client is None:
-            await message.channel.send("音声チャンネルに接続していません。")
+            await message.channel.send('音声チャンネルに接続していません。')
             return
 
         message.guild.voice_client.stop()
         return
 
-    if message.content == "!list-bg":
-        msg = '```\n'
+    if message.content == '!list-bg':
+        files = '```\n'
         for file in os.listdir(libdir):
-            msg += file + '\n'
-        msg += '```'
-        await message.channel.send(msg)
+            if file != '.gitkeep':
+                files += file + '\n'
+        files += '```'
+        await message.channel.send(files)
         return
 
 
     #オウム返し
-    await message.channel.send(message.content)
+    # await message.channel.send(message.content)
+
+# on_messageの定義ここまで
 
 client.run(secret.TOKEN)
